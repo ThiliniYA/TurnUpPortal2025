cb654591916a45c6cdb853cad0b8e7ad8f9811a5
ThiliniYA <thiliniyapa88@gmail.com>
2025-09-22 15:27:21 +1200

    Log in and create time record tests automation

---
+++ TAProgramme.cs
@@ -0,0 +1,112 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.DevTools.V137.Page;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+using System;
+using System.Runtime.CompilerServices;
+
+public class Program
+{
+  
+    public static void Main(string[] args)
+    {
+        // Open chrome browser
+        ChromeOptions options = new ChromeOptions();
+        options.AddUserProfilePreference("profile.password_manager_leak_detection", false);
+        IWebDriver driver = new ChromeDriver(options);
+
+        // Launch TurnUp portal
+        driver.Navigate().GoToUrl("http://horse.industryconnect.io/");
+        driver.Manage().Window.Maximize();
+        Thread.Sleep(1000);
+
+        // Identify Username textbox and enter valid username
+        IWebElement usernameTextbox = driver.FindElement(By.Name("UserName"));
+        usernameTextbox.SendKeys("hari");
+
+        // Identify password textbox and enter valid password
+        IWebElement passwordTextbox = driver.FindElement(By.Id("Password"));
+        passwordTextbox.SendKeys("123123");
+
+        // Identify Login buton and click on it
+        IWebElement loginButton = driver.FindElement(By.XPath("//*[@id=\"loginForm\"]/form/div[3]/input[1]"));
+        loginButton.Click();
+        Thread.Sleep(2000);
+
+        // Check if user has logged in successfully
+        IWebElement helloHari = driver.FindElement(By.XPath("//*[@id=\"logoutForm\"]/ul/li/a"));
+
+        if (helloHari.Text == "Hello hari!")
+        {
+            Console.WriteLine("User has logged in successfylly. Test passed!");
+        }
+
+        else
+        {
+            Console.WriteLine("User has not logged in. Test failled!");
+        }
+
+        // Create a time record
+
+        // navigate to time and material page
+        IWebElement administrationTab = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/a/span"));
+        administrationTab.Click();
+
+        IWebElement timeAndMaterialOption = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/ul/li[3]/a"));
+        timeAndMaterialOption.Click();
+
+        // Click on create new button
+        IWebElement createNewButton = driver.FindElement(By.XPath("//*[@id=\"container\"]/p/a"));
+        createNewButton.Click();
+
+        // Select time from dropdown
+
+        IWebElement typeCodeDropdown = driver.FindElement(By.XPath("//*[@id=\"TimeMaterialEditForm\"]/div/div[1]/div/span[1]/span/span[2]/span"));
+        typeCodeDropdown.Click();
+        Thread.Sleep(1000);
+
+        IWebElement timeOption = driver.FindElement(By.XPath("//*[@id=\"TypeCode_option_selected\"]"));
+        timeOption.Click();
+
+        // Type code into code textbox
+        IWebElement codeTextBox = driver.FindElement(By.Id("Code"));
+        codeTextBox.SendKeys("TA Programme");
+
+        // Type description into Description textbox
+        IWebElement descriptionTextBox = driver.FindElement(By.Id("Description"));
+        descriptionTextBox.SendKeys("This is a description");
+
+        // Type price into price text box
+        IWebElement priceTagOverlap = driver.FindElement(By.XPath("//*[@id=\"TimeMaterialEditForm\"]/div/div[4]/div/span[1]/span/input[1]"));
+        priceTagOverlap.Click();
+
+        IWebElement priceTextbox = driver.FindElement(By.Id("Price"));
+        priceTextbox.SendKeys("12");
+
+        //Click save button
+        IWebElement saveButton = driver.FindElement(By.Id("SaveButton"));
+        saveButton.Click();
+        Thread.Sleep(3000);
+
+        // Check If time recode has been created succussfully.
+        IWebElement goToLastPageButton = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[4]/a[4]/span"));
+        goToLastPageButton.Click();
+
+        IWebElement newCode = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[last()]/td[1]"));
+
+        if (newCode.Text == "TA Programme")
+        {
+            Console.WriteLine("Time record created successfuly!");
+        }
+        else
+        {
+            Console.WriteLine("New time record has not neen created!");
+        }
+    }
+
+    private static void Click()
+    {
+        throw new NotImplementedException();
+    }
+}
